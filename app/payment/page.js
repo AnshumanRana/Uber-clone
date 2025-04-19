ad0a4e7ffafac7cf6eb00cae02085329bf658d54
Anshuman Rana <anshumanrana1404@gmail.com>
2025-04-19 17:03:04 +0530

    Initial commit of Uber clone

--- app/payment/page.js
+++ app/payment/page.js
@@ -1,25 +1,34 @@
-"use client"
-import { Elements } from '@stripe/react-stripe-js';
-import { loadStripe } from '@stripe/stripe-js';
-import { useSearchParams } from 'next/navigation'
-import React from 'react'
-import CheckOutForm from '../Component/Home/CheckOutForm';
+"use client";
 
-function Payment() {
-    const searchParams = useSearchParams();
-    const amount = searchParams.get('amount')
-    const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHER_KEY)
-    const options = {
-        mode: 'payment',
-        amount: Math.round(amount*100),
-        currency: "inr"
-    }
-    
-    return (
-        <Elements stripe={stripePromise} options={options}>
-            <CheckOutForm amount={amount}/>
-        </Elements>
-    )
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import { useSearchParams } from "next/navigation";
+import React, { Suspense } from "react";
+import CheckOutForm from "../Component/Home/CheckOutForm";
+
+function PaymentComponent() {
+  const searchParams = useSearchParams();
+  const amount = searchParams.get("amount");
+
+  const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHER_KEY);
+
+  const options = {
+    mode: "payment",
+    amount: Math.round(amount * 100),
+    currency: "inr",
+  };
+
+  return (
+    <Elements stripe={stripePromise} options={options}>
+      <CheckOutForm amount={amount} />
+    </Elements>
+  );
 }
 
-export default Payment
+export default function Payment() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <PaymentComponent />
+    </Suspense>
+  );
+}
